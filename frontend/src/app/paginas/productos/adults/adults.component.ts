aadbfdd5851b818a1b4dec3d524ba85313aeaa21
sebasc49 <scardonas920@gmail.com>
2024-12-06 20:07:15 -0500

    Actualizacion repo

---
+++ frontend/src/app/paginas/productos/adults/adults.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adults',
+  imports: [],
+  templateUrl: './adults.component.html',
+  styleUrl: './adults.component.css'
+})
+export class AdultsComponent {
+
+}
