883cefa49aa08b6110b6a91a7c7b700b573d6921
sebasc49 <scardonas920@gmail.com>
2024-12-19 18:55:53 -0500

    Panel Administrador

--- frontend/src/app/paginas/login/login-usuarios/login-usuarios.component.ts
+++ frontend/src/app/paginas/login/login-usuarios/login-usuarios.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {ReactiveFormsModule, FormControl, FormGroup} from '@angular/forms';
+import { FooterComponent } from "../../../componentes/footer/footer.component";
+import { NavBarComponent } from "../../../componentes/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-login-usuarios',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, FooterComponent, NavBarComponent],
   templateUrl: './login-usuarios.component.html',
   styleUrl: './login-usuarios.component.css'
 })
