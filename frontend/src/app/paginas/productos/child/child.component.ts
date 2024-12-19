5075455329463c86f962146ab0b4a8253d0d4ec9
sebasc49 <scardonas920@gmail.com>
2024-12-19 12:36:40 -0500

    Creacion de footer

--- frontend/src/app/paginas/productos/child/child.component.ts
+++ frontend/src/app/paginas/productos/child/child.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from '../../../componentes/nav-bar/nav-bar.component';
+import { TarjetaProductoComponent } from '../../../componentes/tarjeta-producto/tarjeta-producto.component';
 
 @Component({
   selector: 'app-child',
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, TarjetaProductoComponent],
   templateUrl: './child.component.html',
   styleUrl: './child.component.css'
 })
